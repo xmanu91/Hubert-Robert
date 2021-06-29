a7f0dd591399272cc208ac3adcbe22fc7161929e
xmanu <xmanu.linguet@gmail.com>
2021-06-29 17:09:28 +0200

    frequent update

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,29 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
+app.use(require('body-parser').json());
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("auth");
 });
 
 app.get("/auth", (req, res) => {
   const password = req.query.password;
   if (password == "azerty") {
-    res.send("connecté");
+    res.redirect(`/home?password=${password}`)
   } else {
     res.redirect("/");
   }
 });
+
+app.get("/home", (req, res) => {
+  if(req.query.password == "azerty"){
+    res.render("index")
+  }else{
+    res.redirect("/")
+  }
+})
 
 app.listen(process.env.port || 3000);
