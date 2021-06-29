97e81bccb6fd63e38e3ef9c057c57e47ca9c6152
Linguet-Emmanuel <emmanuel.linguet@setbrain.fr>
2021-06-29 16:37:34 +0200

    add connect

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 
-app.set('view engine', 'ejs');
+app.use(express.static("public"));
 
+app.set("view engine", "ejs");
 
-app.get('/', (req, res)=> {
-    res.render('index')
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/auth", (req, res) => {
+  const password = req.query.password;
+  if (password == "azerty") {
+    res.send("connecté");
+  } else {
+    res.redirect("/");
+  }
+});
 
 app.listen(process.env.port || 3000);
